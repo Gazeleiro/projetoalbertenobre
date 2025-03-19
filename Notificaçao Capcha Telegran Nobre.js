210d8a7d34665d66a51a0e8b9c18369b0f05acd3
Gazeleiro <gazeleiro@gmail.com>
2025-03-19 17:45:50 -0500

    Update Notificaçao Capcha Telegran Nobre.js

--- Notificaçao Capcha Telegran Nobre.js
+++ Notificaçao Capcha Telegran Nobre.js
@@ -6,7 +6,6 @@
 // @author       Nobre
 // @match        https://*.tribalwars.com.br/*
 // @grant        none
-// @require      https://raw.githubusercontent.com/Gazeleiro/projetoalbertenobre/refs/heads/main/Notifica%C3%A7ao%20Capcha%20Telegran%20Nobre.js
 // @updateURL    https://raw.githubusercontent.com/Gazeleiro/projetoalbertenobre/refs/heads/main/Notifica%C3%A7ao%20Capcha%20Telegran%20Nobre.js
 // @downloadURL  https://raw.githubusercontent.com/Gazeleiro/projetoalbertenobre/refs/heads/main/Notifica%C3%A7ao%20Capcha%20Telegran%20Nobre.js
 // ==/UserScript==
