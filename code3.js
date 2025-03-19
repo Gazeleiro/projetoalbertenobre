43b83d5b6ea8ed99378d1c5319c050c4663c0fc4
Gazeleiro <gazeleiro@gmail.com>
2025-03-19 18:07:26 -0500

    Update code3.js

--- code3.js
+++ code3.js
@@ -1,1 +1,11 @@
-
+// ==UserScript==
+// @name         Ideia 1
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Script Master que carrega automaticamente as atualizações de scripts futuros diretamente do GitHub ou Dropbox.
+// @author       Você
+// @match        https://*.tribalwars.com.br/*
+// @grant        none
+// @updateURL    https://raw.githubusercontent.com/Gazeleiro/projetoalbertenobre/refs/heads/main/code3.js
+// @downloadURL  https://raw.githubusercontent.com/Gazeleiro/projetoalbertenobre/refs/heads/main/code3.js
+// ==/UserScript==
