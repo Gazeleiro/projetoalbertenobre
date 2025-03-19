ec3cef7f81a9cb6a309257b209616a6441d710df
Gazeleiro <gazeleiro@gmail.com>
2025-03-19 08:55:56 -0500

    Create ControleConstrucao.js

---
+++ ControleConstrucao.js
@@ -0,0 +1,10 @@
+{
+  "br134": "Construcao Recursos.js",
+  "br111": "Construcao Recursos.js",
+  "br111": "Construcao Libera Coleta 4.js",
+  "br111": "Construcao Recursos + Mercado para pp.js",
+  "br132": "Construcao Recursos + Mercado para pp.js",
+  "br133": "Construcao Recursos + Mercado para pp.js",
+  "br111": "Construcaoautomatica.js",
+  "br111": "Contrucao Depois Coleta 4.js"
+}
