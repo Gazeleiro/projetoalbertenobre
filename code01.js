62cae36514cbe6cdb410b9c9da6f600c8e9e5504
Gazeleiro <gazeleiro@gmail.com>
2025-03-22 19:22:56 -0500

    Update code01.js

--- code01.js
+++ code01.js
@@ -1,11 +1,18 @@
 // ==UserScript==
-// @name         Ideia 1
+// @name         Auto Reload 10M
 // @namespace    http://tampermonkey.net/
 // @version      1.0
 // @description  Script Master que carrega automaticamente as atualizações de scripts futuros diretamente do GitHub ou Dropbox.
 // @author       Você
-// @match        https://*.tribalwars.com.br/*
+// @match        *://*/*
 // @grant        none
 // @updateURL    https://raw.githubusercontent.com/Gazeleiro/projetoalbertenobre/refs/heads/main/code01.js
 // @downloadURL  https://raw.githubusercontent.com/Gazeleiro/projetoalbertenobre/refs/heads/main/code01.js
 // ==/UserScript==
+(function() {
+    'use strict';
+
+    setTimeout(() => {
+        location.reload();
+    }, 600000); // 600000 ms = 10 minutos
+})();
