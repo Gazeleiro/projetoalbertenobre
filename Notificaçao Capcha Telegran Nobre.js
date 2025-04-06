1a3989d34d90b68968db3ec417bedc79b85e8fe7
Gazeleiro <gazeleiro@gmail.com>
2025-04-06 00:05:27 -0500

    Update Notificaçao Capcha Telegran Nobre.js

--- Notificaçao Capcha Telegran Nobre.js
+++ Notificaçao Capcha Telegran Nobre.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name         Notificação Captcha Telegram + Pagina Inicial
+// @name         Notificação Captcha Telegram
 // @namespace    http://tampermonkey.net/
-// @version      1.2
+// @version      1.3
 // @description  Sempre carrega a versão mais recente do script do Dropbox para notificações de CAPTCHA no Telegram.
 // @author       Nobre
 // @match        https://*.tribalwars.com.br/*
