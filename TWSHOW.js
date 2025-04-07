bd2907f25df58028fcd00d9ab15cff379f49b0c9
Gazeleiro <gazeleiro@gmail.com>
2025-04-07 01:44:09 -0500

    Update TWSHOW.js

--- TWSHOW.js
+++ TWSHOW.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Tw - Show
 // @namespace    http://tampermonkey.net/
-// @version      4.1
+// @version      4.2
 // @include      https://**game.php**
 // @description  Automação para TW
 // @icon         https://i.imgur.com/7WgHTT8.gif
